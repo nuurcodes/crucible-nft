c99d0db4f11400220d431a9c715bd7f1435024e8
nuurcodes <nuurcodes@gmail.com>
2021-03-02 15:56:25 +0000

    Added Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Button from './components/base/button';
+
 function App () {
   return (
     <div className="container" style={{ marginTop: 32 }}>
       <h1 className="text-center" style={{ marginBottom: 24 }}>Alchemist</h1>
-      <button className="button button--primary button--lg">Lets go!</button>
+      <Button size="lg" variant="outline">Outlined</Button>
+      <Button size="lg" style={{ marginLeft: 12 }}>Primary</Button>
+      <Button size="lg" isLoading style={{ marginLeft: 12 }}>Loading</Button>
     </div>
   );
 }
