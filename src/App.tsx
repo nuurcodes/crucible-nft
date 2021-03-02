0b5a79163bd7e8bbab83c54572fa344f6fe052ce
nuurcodes <nuurcodes@gmail.com>
2021-03-02 01:45:02 +0000

    Setup styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ marginTop: 32 }}>
+      <h1 className="text-center" style={{ marginBottom: 24 }}>Alchemist</h1>
+      <button className="button button--primary button--lg">Lets go!</button>
     </div>
   );
 }
